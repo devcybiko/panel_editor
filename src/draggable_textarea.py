58da03308709dc1e07726e08e97f751dff7ce79b
Gregory Smith <greg@agilefrontiers.com>
2025-11-03 12:00:28 -0500

    adding dist.sh

--- src/draggable_textarea.py
+++ src/draggable_textarea.py
@@ -56,7 +56,8 @@ class DraggableTextArea(DraggableWidget, PropertiesWidget, FilebackedWidget, Tex
     def update(self, props=None):
         # GLS - HACK - removing "language" support for now as it causes issues with TextArea base class
         self.props.language = None
-        props.language = None
+        if props is not None:
+            props.language = None
         super().update(props)
         if self.text != self.props.value:
             self.text = self.props.value
